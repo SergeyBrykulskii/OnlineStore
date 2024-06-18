b805861447b14cd2a3a57094954adcb4b7b38401
Sergey Brykulskii <92694237+SergeyBrykulskii@users.noreply.github.com>
2024-06-18 14:59:24 +0300

    OnlineStore-1. added basic project structure (#1)
    * added basic project structure
    
    * projects with tests are placed in a separate folder

---
+++ OnlineStore/OnlineStore.DAL/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnlineStore.DAL.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.InitRepositories();
+    }
+
+    public static void InitRepositories(this IServiceCollection services)
+    {
+    }
+}
