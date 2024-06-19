08b7aab1d8d259d25951766dc51c49c759419638
oleg <olezhka1629@gmail.com>
2024-06-19 19:22:58 +0300

    created db context and models configuration,also created repositories interfaces and implimented some of them

---
+++ OnlineStore/OnlineStore.DAL/Repositories/repositories/OrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.DAL.Repositories.repositories
+{
+    internal class OrderRepository
+    {
+    }
+}
