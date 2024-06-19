c6624d871289ff5ad3d49038fed3cc9185d361cc
Ksenia Antilevskaya <antikevkun@gmail.com>
2024-06-19 14:36:41 +0300

    Create a dto for category

---
+++ OnlineStore/OnlineStore.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineStore.Application.DTOs;
+
+//Dto for obtaining information about category/all categories
+public class CategoryDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class CreateCategoryDto
+{
+    public string Name { get; set; }
+}
+
+public class UpdateCategoryDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+}
