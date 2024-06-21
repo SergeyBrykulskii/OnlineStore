be29e593b3f83f60e6adaf1958bfebac28e22ebb
Ksenia Antilevskaya <antikevkun@gmail.com>
2024-06-21 16:06:17 +0300

    Add OrderProductDto

---
+++ OnlineStore/OnlineStore.Application/DTOs/OrderProductDTOs/UpdateOrderProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore.Application.DTOs.OrderProductDTOs;
+
+public class UpdateOrderProductDto
+{
+    public long Id { get; set; }
+    public long ProductId { get; set; }
+    public long OrderId { get; set; }
+    public int Quantity { get; set; }
+}
