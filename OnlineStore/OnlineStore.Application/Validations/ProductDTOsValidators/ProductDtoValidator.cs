092365b4632d6503b9cf9fbc9e644705df966eea
cutehedgehog <92777569+cutehedgehog@users.noreply.github.com>
2024-06-22 19:32:28 +0300

    Add validators for dtos

---
+++ OnlineStore/OnlineStore.Application/Validations/ProductDTOsValidators/ProductDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using OnlineStore.Application.DTOs.ProductDTOs;
+
+namespace OnlineStore.Application.Validations.ProductDTOsValidators
+{
+	public class ProductDtoValidator : AbstractValidator<ProductDto>
+	{
+		public ProductDtoValidator()
+		{
+			RuleFor(c => c.Id)
+				.NotEmpty().WithMessage("Order id should not be empty");
+			RuleFor(c => c.Name)
+				.NotEmpty().WithMessage("Product name should not be empty");
+			RuleFor(c => c.Name)
+				.MaximumLength(100).WithMessage("Product name is too long");
+			RuleFor(c => c.Price)
+				.GreaterThanOrEqualTo(0).WithMessage("Price should not be negative");
+		}
+	}
+}
