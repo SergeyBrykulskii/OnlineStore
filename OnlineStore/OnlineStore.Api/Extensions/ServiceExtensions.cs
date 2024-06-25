c39ed4edc54db88923f66af4860d2b4c2a225fcb
Sergey Brykulskii <serxio6758sk@gmail.com>
2024-06-25 11:01:18 +0300

    added extension for jwt in main project

--- OnlineStore/OnlineStore.Api/Extensions/ServiceExtensions.cs
+++ OnlineStore/OnlineStore.Api/Extensions/ServiceExtensions.cs
@@ -1,30 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
-using OnlineStore.IdentityServer.ApplicationDbContext;
-using OnlineStore.IdentityServer.Models;
 using System.Text;
 
-namespace OnlineStore.IdentityServer.Extensions;
+namespace OnlineStore.Api.Extensions;
 
 public static class ServiceExtensions
 {
-    public static void ConfigureIdentity(this IServiceCollection services)
-    {
-        var builder = services.AddIdentityCore<User>(o =>
-        {
-            o.Password.RequireDigit = true;
-            o.Password.RequireLowercase = false;
-            o.Password.RequireUppercase = false;
-            o.Password.RequireNonAlphanumeric = false;
-            o.Password.RequiredLength = 10;
-            o.User.RequireUniqueEmail = true;
-        });
-        builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), builder.Services);
-        builder.AddEntityFrameworkStores<AppDbContext>()
-            .AddDefaultTokenProviders();
-    }
-
     public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
     {
         var jwtSettings = configuration.GetSection("JwtSettings");
