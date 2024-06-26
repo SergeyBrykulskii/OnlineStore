161b474147a0b4b030016e908b66d113ea3fa9bb
cutehedgehog <92777569+cutehedgehog@users.noreply.github.com>
2024-06-26 12:16:22 +0300

    Create ActionFilter for validation

---
+++ OnlineStore/OnlineStore.Api/ActionFilters/ValidationFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace OnlineStore.Api.ActionFilters
+{
+    public class ValidationFilterAttribute : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var param = context.ActionArguments.SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
+            if (param == null)
+            {
+                context.Result = new BadRequestObjectResult("Object sent from client is null");
+                return;
+            }
+
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) { }
+    }
+}
