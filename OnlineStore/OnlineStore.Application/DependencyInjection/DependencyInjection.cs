a26118bc29b260c9f2dfa9c5cec29840474a333a
cutehedgehog <92777569+cutehedgehog@users.noreply.github.com>
2024-06-26 12:17:55 +0300

    Refactor validators dependency injection

--- OnlineStore/OnlineStore.Application/DependencyInjection/DependencyInjection.cs
+++ OnlineStore/OnlineStore.Application/DependencyInjection/DependencyInjection.cs
@@ -1,14 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
-using OnlineStore.Application.DTOs.CategoryDTOs;
-using OnlineStore.Application.DTOs.OrderDTOs;
-using OnlineStore.Application.DTOs.OrderProductDTOs;
-using OnlineStore.Application.DTOs.ProductDTOs;
 using OnlineStore.Application.Services.Implementations;
 using OnlineStore.Application.Services.Interfaces;
-using OnlineStore.Application.Validations.CategoryDTOsValidators;
-using OnlineStore.Application.Validations.OrderDTOsValidator;
-using OnlineStore.Application.Validations.OrderProductsDTOsValidators;
 using OnlineStore.Application.Validations.ProductDTOsValidators;
 using System.Reflection;
 
@@ -20,22 +13,7 @@ public static class DependencyInjection
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
-        services.AddScoped<IValidator<CategoryDto>, CategoryDtoValidator>();
-        services.AddScoped<IValidator<CreateCategoryDto>, CreateCategoryDtoValidator>();
-        services.AddScoped<IValidator<UpdateCategoryDto>, UpdateCategoryDtoValidator>();
-
-        services.AddScoped<IValidator<OrderDto>, OrderDtoValidator>();
-        services.AddScoped<IValidator<OrderDetailDto>, OrderDetailDtoValidator>();
-        services.AddScoped<IValidator<CreateOrderDto>, CreateOrderDtoValidator>();
-
-        services.AddScoped<IValidator<OrderProductDto>, OrderProductDtoValidator>();
-        services.AddScoped<IValidator<CreateOrderProductDto>, CreateOrderProductDtoValidator>();
-        services.AddScoped<IValidator<UpdateOrderProductDto>, UpdateOrderProductDtoValidator>();
-
-        services.AddScoped<IValidator<ProductDto>, ProductDtoValidator>();
-        services.AddScoped<IValidator<ProductDetailDto>, ProductDetailDtoValidator>();
-        services.AddScoped<IValidator<CreateProductDto>, CreateProductDtoValidator>();
-        services.AddScoped<IValidator<UpdateProductDto>, UpdateProductDtoValidator>();
+        services.AddValidatorsFromAssemblyContaining<ProductDtoValidator>();
 
         InitServices(services);
     }
