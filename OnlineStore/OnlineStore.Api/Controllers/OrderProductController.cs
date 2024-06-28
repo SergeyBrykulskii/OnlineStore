af6cab4f742a91b59b867037c3e7c2b772d30263
oleg <olezhka1629@gmail.com>
2024-06-28 11:09:09 +0300

    code clean up

--- OnlineStore/OnlineStore.Api/Controllers/OrderProductController.cs
+++ OnlineStore/OnlineStore.Api/Controllers/OrderProductController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Application.DTOs.OrderProductDTOs;
+using OnlineStore.Application.DTOs.ProductDTOs;
+using OnlineStore.Application.Result;
+using OnlineStore.Application.Services.Interfaces;
 
 namespace OnlineStore.Api.Controllers
 {
@@ -7,5 +10,67 @@ namespace OnlineStore.Api.Controllers
     [ApiController]
     public class OrderProductController : ControllerBase
     {
+        private readonly IOrderProductService _orderProductService;
+        public OrderProductController(IOrderProductService orderProductService)
+        {
+            _orderProductService = orderProductService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CollectionResult<OrderProductDto>>> GetProductsInOrder(long orderId)
+        {
+            var result = await _orderProductService.GetProductsByOrder(orderId);
+
+            if (result == null || result.Data == null)
+            {
+                return NotFound(new { result.ErrorMessage });
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResult<CreateOrderProductDto>>> CreateOrderProduct([FromBody] CreateOrderProductDto orderProductDto)
+        {
+            var result = await _orderProductService.AddProductToOrderAsync(orderProductDto);
+
+            if (result.Data == null)
+            {
+                return BadRequest(new { result.ErrorMessage });
+            }
+
+            return CreatedAtAction(nameof(CreateOrderProduct), result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BaseResult<UpdateProductDto>>> UpdateOrderProduct(long id, [FromBody] UpdateOrderProductDto orderProductDto, CancellationToken cancellationToken)
+        {
+            if (id != orderProductDto.Id)
+            {
+                return BadRequest(new { Message = "ID doessn't exist" });
+            }
+
+            var result = await _orderProductService.UpdateProductQuantityInOrderAsync(orderProductDto);
+
+            if (result.Data == null)
+            {
+                return NotFound(new { result.ErrorMessage });
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BaseResult<long>>> DeleteOrderProduct(long id)
+        {
+            var result = await _orderProductService.RemoveProductFromOrderAsync(id);
+
+            if (result.Data == 0)
+            {
+                return NotFound(new { result.ErrorMessage });
+            }
+
+            return Ok(result);
+        }
     }
 }
--- OnlineStore/OnlineStore.Api/Controllers/ProductController.cs
+++ OnlineStore/OnlineStore.Api/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Application.DTOs.CategoryDTOs;
+using OnlineStore.Application.DTOs.ProductDTOs;
+using OnlineStore.Application.Result;
+using OnlineStore.Application.Services.Interfaces;
 
 namespace OnlineStore.Api.Controllers
 {
@@ -7,5 +10,93 @@ namespace OnlineStore.Api.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+        public ProductController(IProductService service)
+        {
+            _productService = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CollectionResult<ProductDto>>> GetProducts()
+        {
+            var result = await _productService.GetAllProductsAsync();
+
+            if (result == null || result.Data == null)
+            {
+                return NotFound(new { Message = "Products not found." });
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BaseResult<CategoryDto>>> GetProduct(long id)
+        {
+            var result = await _productService.GetProductByIdAsync(id);
+
+            if (result.Data == null)
+            {
+                return NotFound(new { result.ErrorMessage });
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<BaseResult<CategoryDto>>> GetProductsByCategory(long categoryId)
+        {
+            var result = await _productService.GetProductsByCategoryAsync(categoryId);
+
+            if (result.Data == null)
+            {
+                return NotFound(new { result.ErrorMessage });
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResult<CreateProductDto>>> CreateProduct([FromBody] CreateProductDto productDto)
+        {
+            var result = await _productService.CreateProductAsync(productDto);
+
+            if (result.Data == null)
+            {
+                return BadRequest(new { result.ErrorMessage });
+            }
+
+            return CreatedAtAction(nameof(CreateProduct), new { name = productDto.Name }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BaseResult<UpdateProductDto>>> UpdateProduct(long id, [FromBody] UpdateProductDto productDto, CancellationToken cancellationToken)
+        {
+            if (id != productDto.Id)
+            {
+                return BadRequest(new { Message = "ID mismatch." });
+            }
+
+            var result = await _productService.UpdateProductAsync(productDto);
+
+            if (result.Data == null)
+            {
+                return NotFound(new { result.ErrorMessage });
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BaseResult<long>>> DeleteProduct(long id)
+        {
+            var result = await _productService.DeleteProductAsync(id);
+
+            if (result.Data == 0)
+            {
+                return NotFound(new { result.ErrorMessage });
+            }
+
+            return Ok(result);
+        }
     }
 }
